5a97b4c14418629fcd93200687e437b82b560a99
zwan669 <zixunqwq@gmail.com>
2022-05-12 16:33:38 +0800

    I dp II. greedy

---
+++ 剑指offer/14.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(cuttingRope(56))
+	fmt.Println(cuttingRope2(56))
+}
+
+/*
+时间复杂度：O(n^2)
+空间复杂度：O(n)
+转移方程：dp[i] = max(dp[i], max(j*(i-j), j*dp[i-j]))
+*/
+func cuttingRope(n int) int {
+	dp := make([]int, n+1)
+	dp[2] = 1 // 初始值
+	for i:=3; i<=n; i++ {
+		for j:=2; j<i; j++ {
+			max := 0
+			if j*(i-j) > j*dp[i-j] {
+				max = j*(i-j)
+			} else {
+				max = j*dp[i-j]
+			}
+			if dp[i] < max {
+				dp[i] = max
+			}
+		}
+	}
+	return dp[n]
+}
+
+// 贪心
+
+func cuttingRope2(n int) int {
+	if n < 4 {
+		return n-1
+	}
+	result := 1
+	switch n%3 {
+	case 0:
+		for i:=0; i<n/3; i++ {
+			result = result * 3 % (1e9+7)
+		}
+	case 1:
+		for i:=0; i<n/3-1; i++ {
+			result = result * 3 % (1e9+7)
+		}
+		result = result * 2 * 2 % (1e9+7)
+	case 2:
+		for i:=0; i<n/3; i++ {
+			result = result * 3 % (1e9+7)
+		}
+		result = result * 2 % (1e9+7)
+	}
+	return result
+}
