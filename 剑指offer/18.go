ed62bf3c8978356fb22f9ce2e9866905da012fab
zwan669 <zixunqwq@gmail.com>
2022-05-15 00:55:01 +0800

    链表双指针

--- 剑指offer/18.go
+++ 剑指offer/18.go
@@ -1,1 +1,34 @@
 package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 双指针
+func deleteNode(head *ListNode, val int) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	ptr := head
+	next := ptr.Next
+	if ptr.Val == val {
+		return next
+	}
+	for next != nil {
+		if next.Val == val {
+			ptr.Next = next.Next
+		}
+		ptr = next
+		next = next.Next
+	}
+	return head
+}
